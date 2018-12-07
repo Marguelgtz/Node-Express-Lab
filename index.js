26eb0e700568425a4453b4b246e2bf3e2c9ea445
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-12-07 15:15:25 -0600

    getting all posts done

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+
+const server = express();
+const PORT = 4000;
 // add your server code starting here
+
+server
+  .get('/api/posts', (req, res) => {
+    db
+      .find()
+      .then(posts => {
+        res.json(posts)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({message: 'failed to get posts'})
+      })
+  })
+
+// Listening 
+server.listen(PORT, () => {
+  console.log(`Server runnign on port ${PORT}`)
+})
