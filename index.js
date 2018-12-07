cd6a214a6aac62bde3e556feb1ffaf3a29d75a8d
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-12-07 15:40:50 -0600

    get post by id done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const server = express();
 const PORT = 4000;
 // add your server code starting here
 
+// Get all posts
 server
   .get('/api/posts', (req, res) => {
     db
@@ -18,6 +19,28 @@ server
         res
           .status(500)
           .json({message: 'failed to get posts'})
+      })
+  })
+
+server
+  .get('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db
+      .findById(id) 
+      .then(post => {
+        console.log(post);
+        if(post[0]) {
+          res.json(post[0])
+        } else {
+          res
+            .status(404)
+            .json({message: "Post does not exist"})
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({message: 'Failed to get post'})
       })
   })
 
